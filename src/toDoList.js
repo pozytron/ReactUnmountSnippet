611907501afb9e1fd528a1aa22e752cf57d2b00c
Michał Tymuła <michal.tymula@coderslab.pl>
2019-01-19 13:48:26 +0100

    unmount trigger

--- src/toDoList.js
+++ src/toDoList.js
@@ -3,14 +3,29 @@ import ReactDOM from "react-dom";
 
 import "./layout.css";
 class Menu extends React.Component {
-  render() {}
+  handleClick = () => {
+    if (typeof this.props.onUnmount === "function") {
+      console.log("dasdasd");
+    }
+  };
+  render() {
+    return (
+      <nav>
+        <button>Dodaj</button>
+        <button>Usuń</button>
+        <button>Edytuj</button>
+        <button onClick={this.handleClick}>Odmontuj</button>
+      </nav>
+    );
+  }
 }
 
 export default class ToDoList extends React.Component {
+  unmountCallback = () => {};
   render() {
     return (
       <div className="list-container">
-        <Menu />
+        <Menu onUnmount={() => this.unmountCallback()} />
         <p>THIS IS A CONTAINER FOR LIST </p>
       </div>
     );
